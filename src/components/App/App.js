95b7386e5337adc3576ce0120b7894df41fd2250
lourdesmendoza <lourd.mendoza@gmail.com>
2021-07-21 10:07:29 -0600

    Add styling to page and each card displayed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -34,7 +34,7 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
-          <AllReservations allReservations={this.state.reservations}/>
+          {this.state.reservations.length && <AllReservations allReservations={this.state.reservations}/>}
           
         </div>
       </div>
--- src/components/OneReservation/OneReservation.js
+++ src/components/OneReservation/OneReservation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './OneReservation.css'
 
 const OneReservation = ({id, name, date, time, number, cancelReservation}) => {
     return (
