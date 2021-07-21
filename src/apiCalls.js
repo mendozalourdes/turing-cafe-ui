43dc28f44e15c52feaa1ddd4f16ad63b1766e813
lourdesmendoza <lourd.mendoza@gmail.com>
2021-07-21 09:31:17 -0600

    Create error handling function for apicalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,10 +2,13 @@ const baseURL = 'http://localhost:3001/api/v1'
 
 let apiCalls = {
 
-
-
-
-
+    checkForErr(response) {
+        if(!response.ok) {
+            return "Oops, we are having technical issues at the moment."
+        } else {
+            return response.json();
+        }
+    },
 
 
     fetchAPIData(type) {
